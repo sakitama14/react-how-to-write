6b5df635abf7a9a3ead0b147c9500e49bf1479f9
sakitama14 <104491362+sakitama14@users.noreply.github.com>
2023-08-01 08:47:00 +0900

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>あいうえお</h1>
+      <p>かきくけこ</p>
+      <button>ボタンです</button>
+    </>
+  );
+};
+
+export default App;
