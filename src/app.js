35c71078ecfea0bd9929b0ab392714609548c184
sakitama14 <104491362+sakitama14@users.noreply.github.com>
2023-08-02 08:53:50 +0900

    Reactでのイベントやスタイルの扱い方を知る

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 const App = () => {
+  const onClickButton = () => alert("クリックされました");
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px",
+    fontWeight: "bold"
+  };
+
   return (
     <>
-      <h1>あいうえお</h1>
-      <p>かきくけこ</p>
-      <button>ボタンです</button>
+      <h1 style={{ color: "red" }}>あいうえお</h1>
+      <p style={contentStyle}>かきくけこ</p>
+      <button onClick={onClickButton}>ボタンです</button>
     </>
   );
 };
